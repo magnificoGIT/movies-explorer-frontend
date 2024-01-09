119cd4832e6169c9b20fdb2e80f9699af73806c3
Igor Klein <savonarola1452@yandex.ru>
2024-01-09 14:13:56 +0200

    Добавить переход на предыдущую страницу при клике на кнопку на странице PageNotFound

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function PageNotFound() {
+  const navigate = useNavigate();
+   
   return (
     <section className="not-found">
       <h2 className="not-found__title">404</h2>
       <p className="not-found__paragraph">Страница не найдена</p>
-      <Link to="/" className="not-found__link">
+      <button className="not-found__link" onClick={() => navigate(-1)}>
         Назад
-      </Link>
+      </button>
     </section>
   );
 }
